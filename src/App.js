b9b4f9fb04955495c567308f35b4850fba541ae7
zaim-br24 <71899665+zaim-br24@users.noreply.github.com>
2021-04-13 08:14:14 +0000

    add the shop page and the collection-preview and collection-item with style.scss , modified app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
 import "./App.css";
-import { Link, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
-const JacketsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Jackets Page</h1>
-    </div>
-  );
-};
-
+import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage}></Route>
-      <Route exact path="/shop/hats" component={HatsPage}></Route>
-      <Route path="/shop/jackets" component={JacketsPage}></Route>
+      <Route path="/shop" component={ShopPage}></Route>
     </div>
   );
 }
---
+++ src/components/collectionPreview/collection-preview.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./collection-preview.style.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items.slice(0, 4).map(({ id, ...otherItemProps }) => (
+        <CollectionItem key={id} {...otherItemProps} />
+      ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
